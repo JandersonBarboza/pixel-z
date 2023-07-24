466b55ccbf89a185bc55d2cf03ece12853d25836
JanBarboza <janderson.barboza@optimuz.com.br>
2023-07-23 22:43:17 -0300

    add resposividade (provisorio)

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import { Col, Row, Card, MenuProps, Menu } from 'antd';
-import Banner from '../../assets/banner/Banner_Curso-PixelZ-1280.png'
-import './scroll.css'
+import Banner from '../../assets/banner/Banner_BG.png';
+import Logo from '../../assets/banner/Logo_Curso.svg';
+import './scroll.css';
 import { useState } from 'react';
 import { Conteudo } from '../Conteudo/Conteudo';
 import { items, rootSubmenuKeys } from '../Conteudo/MenuList';
@@ -10,12 +11,14 @@ const headerStyle: React.CSSProperties = {
     height: '25vh',
     display: 'flex',
     justifyContent: 'center',
+    alignItems: 'center',
     background: '#686f99',
-    borderBottom: '4px dashed #404973',
+    backgroundImage: `url(${Banner})`
+    // borderBottom: '4px dashed #404973',
 };
 
 const bodyStyle: React.CSSProperties = {
-    padding: '2.5vh 0 2.5vh 16px',
+    padding: '2.5vh 16px',
     height: '70vh',
     color: '#fff',
     background: '#686f99',
@@ -58,30 +61,30 @@ export function Home() {
     }
 
     return (
-        <div style={{ width: '80 rem' }}>
+        <div>
             <Row>
                 <Col span={24} style={headerStyle}>
                     <div>
-                        <img src={Banner} height={'100%'} />
+                        <center><img src={Logo} width='256rem' style={{}} /></center>
                     </div>
                 </Col>
-                <Col span={16} style={bodyStyle}>
-                    <Card className='scrollStyled' style={{ height: '100%', overflowY: 'auto' }}>
-                        <Conteudo codigo={content} />
-                    </Card>
-                </Col>
-                <Col span={8} style={CardStyle}>
-                    <Menu
-                        className='scrollStyled'
-                        mode="inline"
-                        openKeys={openKeys}
-                        onOpenChange={onOpenChange}
-                        items={items}
-                        style={{ width: 'auto', height: '65vh', borderRadius: '8px', overflowY: 'auto' }}
-                        onClick={(e)=>handleContent(e.key)}
-                    />
-                </Col>
-                <Col span={24} style={footerStyle}>
+                    <Col xs={{ order: 2, span: 24 }} md={{ order: 1, span: 16 }} style={bodyStyle}>
+                        <Card className='scrollStyled' style={{ height: '100%', overflowY: 'auto' }}>
+                            <Conteudo codigo={content} />
+                        </Card>
+                    </Col>
+                    <Col xs={{ order: 1, span: 24 }} md={{ order: 2, span: 8 }} style={bodyStyle}>
+                        <Menu
+                            className='scrollStyled'
+                            mode="inline"
+                            openKeys={openKeys}
+                            onOpenChange={onOpenChange}
+                            items={items}
+                            style={{ height: '100%', borderRadius: '8px', overflowY: 'auto' }}
+                            onClick={(e) => handleContent(e.key)}
+                        />
+                    </Col>
+                <Col xs={{ order: 3 }} span={24} style={footerStyle}>
                     <div>JanDev | @GGCoffee | 2023</div>
                 </Col>
             </Row>
